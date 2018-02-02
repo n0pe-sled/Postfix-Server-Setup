98dec64d4f2eb13ee16d24fbbd3f404a8d990c6f
Abraxas <stewart.olson@gmail.com>
2018-02-02 11:49:42 -0800

    Fixed minor unzip issue
    Added `-d` flag to unzip and changed cd to match directory.

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -526,8 +526,8 @@ setupSSH(){
 function Install_GoPhish {
 	apt-get install unzip > /dev/null 2>&1
 	wget https://github.com/gophish/gophish/releases/download/v0.5.0/gophish-v0.5.0-linux-64bit.zip
-	unzip gophish-v0.5.0-linux-64bit.zip
-	cd gophish-v0.5.0-linux-64bit
+	unzip gophish-v0.5.0-linux-64bit.zip -d gophish
+	cd gophish
         sed -i 's/"listen_url" : "127.0.0.1:3333"/"listen_url" : "0.0.0.0:3333"/g' config.json
 	read -r -p "Do you want to add an SSL certificate to your GoPhish? [y/N] " response
 	case "$response" in
