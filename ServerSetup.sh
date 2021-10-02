19c57ae6427ed6d93690ee0b8215f34b23387c81
Muhamohsen <75082239+Muhamohsen@users.noreply.github.com>
2021-10-02 04:27:57 -0700

    fix gophish instll function
    add a dir gophish and unzip there

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -548,8 +548,9 @@ setupSSH(){
 function Install_GoPhish {
 	apt-get install unzip > /dev/null 2>&1
 	wget https://github.com/gophish/gophish/releases/download/v0.11.0/gophish-v0.11.0-linux-64bit.zip
-	unzip gophish-v0.11.0-linux-64bit.zip
-	cd gophish-v0.11.0-linux-64bit
+	mkdir ./gophish
+	unzip gophish-v0.11.0-linux-64bit.zip -d ./gophish
+	cd gophish
         sed -i 's/"listen_url" : "127.0.0.1:3333"/"listen_url" : "0.0.0.0:3333"/g' config.json
 	read -r -p "Do you want to add an SSL certificate to your GoPhish? [y/N] " response
 	case "$response" in
